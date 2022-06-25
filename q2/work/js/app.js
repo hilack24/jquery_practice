ef4d3cbd88218bcdceaaa6437387569fc6c2782f
hilack24 <noda1945@gmail.com>
2022-06-25 10:09:46 +0900

    20220624

---
+++ q2/work/js/app.js
@@ -0,0 +1,19 @@
+$(function() {
+
+  $('.modal_open_button').click(function() {
+    $('.modal_win').fadeIn();
+  });
+  $('.modal_close_button').click(function() {
+    $('.modal_win').fadeOut();
+  });
+
+});
+
+
+
+
+
+
+
+
+
